5b9b54f5d62ed989c7f77906a770d69e31c1faa4
Thagon <alex1901schmidt@gmail.com>
2022-02-01 15:37:10 +0100

    new template

---
+++ src/Body.tsx
@@ -0,0 +1,10 @@
+import { Typography } from '@mui/material';
+import React from 'react';
+
+export default function Body () {
+  return(
+      <Typography>
+          1234
+      </Typography>
+  );
+}
